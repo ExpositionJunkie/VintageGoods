247d598ba2f1f45cf5c489808d2b7abffb71e7dc
Roz Albrecht <rosalindavalbrecht@gmail.com>
2021-04-02 20:12:29 -0700

    Baby steps - Resources for Etsy API

---
+++ linking.js
@@ -0,0 +1,27 @@
+// General API Video Tutorial
+// https://www.youtube.com/watch?v=ecT42O6I_WI
+
+// Etsy API documentation
+// https://www.etsy.com/developers/documentation/getting_started/api_basics
+
+// Special documentation for javascript access:
+// https://www.etsy.com/developers/documentation/getting_started/jsonp
+
+// Jquery documentation:
+// https://www.etsy.com/developers/documentation/resources/jquery
+
+/* APP NAME eCommerce Vintage Goods Storefront
+KEYSTRING 6ldxmjbulo45wo6otj054ehd
+SHARED SECRET ctbjil51z6 */
+
+api_key = "6ldxmjbulo45wo6otj054ehd";
+
+// listings:
+// https://www.etsy.com/developers/documentation/reference/listing
+$(document).ready(function() {
+
+})
+
+function getData() {
+
+}
